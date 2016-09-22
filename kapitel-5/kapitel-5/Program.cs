7e7feb70a506bfdb451b1991e2bb24a72a354f21
Dominic Katzwinkel <dominic.katzwinkel@outlook.de>
2016-09-22 14:47:54 +0200

    Kapitel 5.1.4 Switch

--- kapitel-5/kapitel-5/Program.cs
+++ kapitel-5/kapitel-5/Program.cs
@@ -10,22 +10,44 @@ namespace kapitel_5
     {
         static void Main(string[] args)
         {
-            double kapital, prozentsatz, zinsen;
+            //double kapital, prozentsatz, zinsen;
 
-            Console.Write("Bitte dein Kapital eingeben: ");
-            kapital = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Bitte dein Kapital eingeben: ");
+            //kapital = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Bitte deine Zinsen eingeben: ");
-            zinsen = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Bitte deine Zinsen eingeben: ");
+            //zinsen = Convert.ToDouble(Console.ReadLine());
 
-            if (kapital == 0)
+            //if (kapital == 0)
+            //{
+            //    Console.WriteLine("Fehler! Dein Kapital darf nicht 0 sein.");
+            //}
+            //else
+            //{
+            //    prozentsatz = zinsen * 100 / kapital;
+            //    Console.WriteLine("Dein Prozentsatz liegt bei: {0}%", prozentsatz);
+            //}
+            //Console.ReadKey();
+
+            string auswahl;
+
+            Console.WriteLine("Auswahl 1");
+            Console.WriteLine("Auswahl 2");
+            Console.WriteLine("Auswahl 3");
+            Console.Write("Bitte geben Sie Ihre Auswahl ein: ");
+            auswahl = Console.ReadLine();
+
+            switch (auswahl)
             {
-                Console.WriteLine("Fehler! Dein Kapital darf nicht 0 sein.");
-            }
-            else
-            {
-                prozentsatz = zinsen * 100 / kapital;
-                Console.WriteLine("Dein Prozentsatz liegt bei: {0}%", prozentsatz);
+                case "1":
+                    Console.WriteLine("Deine Auswahl war die 1");
+                    break;
+                case "2":
+                    Console.WriteLine("Deine Auswahl war die 2");
+                    break;
+                case "3":
+                    Console.WriteLine("Deine Auswahl war die 3");
+                    break;
             }
             Console.ReadKey();
         }
