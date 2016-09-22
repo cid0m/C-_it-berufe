032eb665b7a15d9d25eb8f1b2aead1876f82c59a
Dominic Katzwinkel <dominic.katzwinkel@outlook.de>
2016-09-22 14:22:57 +0200

    Test

---
+++ kapitel-3/kapitel-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kapitel_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            double d = 1.25;
+            string s = "Hallo";
+            char c = 'x';
+
+            //Console.WriteLine(i);
+            //Console.WriteLine("Integer: " + i + " double: " + d);
+            //Console.WriteLine("Zeichenkette: " + s + "Zeichen: " + c);
+            //Console.ReadKey();
+
+            Console.WriteLine("Integer: {0}  double: {1} \a", i, d);
+            Console.WriteLine("Zeichenkette: {0} Zeichen: {1} \a", s, c);
+            Console.ReadKey();
+        }
+    }
+}
